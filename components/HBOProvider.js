23b0c0a69193276923ec406a5870e20c89e6ce75
Eduardo Orozco <eddthedev@Eddthedevs-MacBook-Pro.local>
2023-12-12 11:08:41 -0600

    passing in state from HBOProvider to creat page

--- components/HBOProvider.js
+++ components/HBOProvider.js
@@ -7,10 +7,18 @@ export function useStateContext(){
 }
 
 export function HBOProvider({children}){
+    const [user,setUser] = useState('');
+    const defaultUserImg = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRF7ExHtiKhVddHOQW9Qki1wk4dC_ZbA3SJLQ&usqp=CAU'
+    const createUserAction = (e) => {
+        setUser(e.target.value)
+        console.log(user)
+    }
     return (
         <StateContext.Provider
         value={{
-            test:'Eddie'
+            user,
+            createUserAction,
+            defaultUserImg
         }}
         >
             {children}
